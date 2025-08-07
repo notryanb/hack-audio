befaf4610ee9d797a27267aeef2a018fc1c2cd70
Ryan Blecher <notryanb@gmail.com>
2025-08-06 22:50:33 -0400

    add stereo visualizer / goniometer

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use nih_plug::prelude::*;
 use nih_plug_egui::{
-    EguiState, create_egui_editor, egui, egui::Vec2, resizable_window::ResizableWindow, widgets,
+    EguiState, create_egui_editor, egui,
+    egui::emath,
+    egui::{Color32, Pos2, Rect, Stroke, Vec2, vec2},
+    resizable_window::ResizableWindow,
+    widgets,
 };
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use triple_buffer::TripleBuffer;
 
 #[derive(Clone, Enum, PartialEq)]
 pub enum Fx {
@@ -14,6 +19,9 @@ pub enum Fx {
 
     #[id = "mid-side-decode"]
     MidSideDecode,
+
+    #[id = "goniometer"]
+    Goniometer,
 }
 
 impl Fx {
@@ -21,12 +29,14 @@ impl Fx {
         match fx {
             Fx::Panning => 0.0,
             Fx::MidSideEncode => 1.0,
-            Fx::MidSideDecode=> 2.0,
+            Fx::MidSideDecode => 2.0,
+            Fx::Goniometer => 3.0,
         }
     }
 
     pub fn from_f32(i: f32) -> Self {
         match i {
+            3.0 => Fx::Goniometer,
             2.0 => Fx::MidSideDecode,
             1.0 => Fx::MidSideEncode,
             _ => Fx::Panning,
@@ -67,10 +77,26 @@ impl PanningMode {
     }
 }
 
+pub struct OutputBuffer {
+    pub left: [f32; 48000],
+    pub right: [f32; 48000],
+}
+
+impl Default for OutputBuffer {
+    fn default() -> Self {
+        Self {
+            left: [0.0; 48000],
+            right: [0.0; 48000],
+        }
+    }
+}
+
 /// This is mostly identical to the gain example, minus some fluff, and with a GUI.
 pub struct HackAudio {
     params: Arc<PluginParams>,
     ui_state: UiState,
+    goniometer_input: triple_buffer::Input<OutputBuffer>,
+    goniometer_output: Arc<Mutex<triple_buffer::Output<OutputBuffer>>>,
 }
 
 #[derive(Params)]
@@ -95,8 +121,11 @@ pub struct PluginParams {
 
 impl Default for HackAudio {
     fn default() -> Self {
+        let (goniometer_input, goniometer_output) = TripleBuffer::default().split();
         Self {
             params: Arc::new(PluginParams::default()),
+            goniometer_input: goniometer_input,
+            goniometer_output: Arc::new(Mutex::new(goniometer_output)),
             ui_state: UiState {},
         }
     }
@@ -123,10 +152,7 @@ impl Default for PluginParams {
             mid_side_enc_stereo_width: FloatParam::new(
                 "Stereo Width",
                 0.0,
-                FloatRange::Linear {
-                    min: 0.0,
-                    max: 2.0,
-                },
+                FloatRange::Linear { min: 0.0, max: 2.0 },
             )
             .with_value_to_string(formatters::v2s_f32_rounded(2)),
         }
@@ -166,6 +192,7 @@ impl Plugin for HackAudio {
     fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
         let params = self.params.clone();
         let egui_state = params.editor_state.clone();
+        let goniometer = self.goniometer_output.clone();
         create_egui_editor(
             self.params.editor_state.clone(),
             self.ui_state.clone(),
@@ -212,6 +239,17 @@ impl Plugin for HackAudio {
                                 {
                                     setter.begin_set_parameter(&params.selected_fx);
                                     setter.set_parameter(&params.selected_fx, Fx::MidSideDecode);
+                                    setter.end_set_parameter(&params.selected_fx);
+                                }
+                                if ui
+                                    .add(egui::widgets::SelectableLabel::new(
+                                        *selected_fx == Fx::Goniometer,
+                                        "Stereo Visualizer",
+                                    ))
+                                    .clicked()
+                                {
+                                    setter.begin_set_parameter(&params.selected_fx);
+                                    setter.set_parameter(&params.selected_fx, Fx::Goniometer);
                                     setter.end_set_parameter(&params.selected_fx);
                                 }
                             });
@@ -266,14 +304,90 @@ impl Plugin for HackAudio {
                                 ui.label("Pan");
                                 ui.add(widgets::ParamSlider::for_param(&params.pan, setter));
                             }
-                            Fx::MidSideEncode => { 
-                                ui.label("MidSideEncode"); 
+                            Fx::MidSideEncode => {
+                                ui.label("MidSideEncode");
                                 ui.separator();
-                                
+
                                 ui.label("Stereo Width");
-                                ui.add(widgets::ParamSlider::for_param(&params.mid_side_enc_stereo_width, setter));
+                                ui.add(widgets::ParamSlider::for_param(
+                                    &params.mid_side_enc_stereo_width,
+                                    setter,
+                                ));
                             }
-                            Fx::MidSideDecode => { ui.label("MidSideDecode"); }
+                            Fx::MidSideDecode => {
+                                ui.label("MidSideDecode");
+                            }
+                            Fx::Goniometer => {
+                                ui.label("Stereo Visualizer (Goniometer)");
+                                // TODO - draw goniometer component here
+                                // and then read data from the goniometer buffer into the component
+                                let axis_color = Color32::from_additive_luminance(196);
+
+                                let desired_size = ui.available_width() * vec2(0.8, 0.8);
+                                let (_id, rect) = ui.allocate_space(desired_size);
+                                // let mut shapes = vec![];
+
+                                let to_screen = emath::RectTransform::from_to(
+                                    Rect::from_x_y_ranges(-1.0..=1.0, -1.0..=1.0),
+                                    rect,
+                                );
+
+                                // Draw the measurement axis
+                                let top_left = rect.left_top();
+                                let top_right = rect.right_top();
+                                let bottom_left = rect.left_bottom();
+                                let bottom_right = rect.right_bottom();
+                                let center_top = rect.center_top();
+                                let center_bottom = rect.center_bottom();
+
+                                ui.painter().line_segment(
+                                    [top_left, bottom_right],
+                                    Stroke::new(1.0, axis_color),
+                                );
+
+                                ui.painter().line_segment(
+                                    [top_right, bottom_left],
+                                    Stroke::new(1.0, axis_color),
+                                );
+
+                                ui.painter().line_segment(
+                                    [center_top, center_bottom],
+                                    Stroke::new(1.0, axis_color),
+                                );
+
+                                ui.painter().circle_stroke(
+                                    rect.center(),
+                                    rect.width() / 2.0,
+                                    Stroke::new(1.0, axis_color),
+                                );
+
+                                {
+                                    // TODO - I'd like to pull the shape map
+                                    // and paint out of the lock. I should only be locking the read
+                                    // and then unlocking after the data is now available
+                                    let lock = goniometer.try_lock();
+                                    if let Ok(mut mutex) = lock {
+                                        let data = mutex.read();
+
+                                        let shapes = data
+                                            .left
+                                            .into_iter()
+                                            .zip(data.right)
+                                            .map(|(left, right)| {
+                                                let pos = Pos2 { x: left, y: right };
+                                                let screen_center = to_screen * pos;
+                                                egui::epaint::Shape::circle_filled(
+                                                    screen_center,
+                                                    1.0,
+                                                    Color32::LIGHT_BLUE,
+                                                )
+                                            })
+                                            .collect::<Vec<egui::epaint::Shape>>();
+
+                                        ui.painter().extend(shapes);
+                                    }
+                                }
+                            }
                         });
                     });
             },
@@ -301,8 +415,35 @@ impl Plugin for HackAudio {
             Fx::Panning => panning_plugin_process(buffer, &self.params),
             Fx::MidSideEncode => mid_side_encode_plugin_process(buffer, &self.params),
             Fx::MidSideDecode => mid_side_decode_plugin_process(buffer, &self.params),
+            Fx::Goniometer => {
+                goniometer_plugin_process(buffer, &self.params, &mut self.goniometer_input)
+            }
         }
     }
+}
+
+pub fn goniometer_plugin_process(
+    buffer: &mut Buffer,
+    _params: &Arc<PluginParams>,
+    goniometer_input: &mut triple_buffer::Input<OutputBuffer>,
+) -> ProcessStatus {
+    let mut ui_buffer = OutputBuffer::default();
+    let num_samples = buffer.samples();
+    let output = buffer.as_slice();
+
+    for sample_idx in 0..num_samples {
+        let l = output[0][sample_idx];
+        let r = output[1][sample_idx];
+        let radius = ((l * l) + (r * r)).sqrt();
+        let angle = r.atan2(l) + (std::f32::consts::PI / 4.0);
+
+        ui_buffer.left[sample_idx] = radius * angle.cos();
+        ui_buffer.right[sample_idx] = radius * angle.sin();
+    }
+
+    goniometer_input.write(ui_buffer);
+
+    ProcessStatus::Normal
 }
 
 /// Linear panning from Hack Audio Book
@@ -339,7 +480,10 @@ pub fn panning_plugin_process(buffer: &mut Buffer, params: &Arc<PluginParams>) -
     ProcessStatus::Normal
 }
 
-pub fn mid_side_encode_plugin_process(buffer: &mut Buffer, params: &Arc<PluginParams>) -> ProcessStatus {
+pub fn mid_side_encode_plugin_process(
+    buffer: &mut Buffer,
+    params: &Arc<PluginParams>,
+) -> ProcessStatus {
     let num_samples = buffer.samples();
     let stereo_width = params.mid_side_enc_stereo_width.value();
     let output = buffer.as_slice();
@@ -355,7 +499,10 @@ pub fn mid_side_encode_plugin_process(buffer: &mut Buffer, params: &Arc<PluginPa
     ProcessStatus::Normal
 }
 
-pub fn mid_side_decode_plugin_process(buffer: &mut Buffer, _params: &Arc<PluginParams>) -> ProcessStatus {
+pub fn mid_side_decode_plugin_process(
+    buffer: &mut Buffer,
+    _params: &Arc<PluginParams>,
+) -> ProcessStatus {
     let num_samples = buffer.samples();
     let output = buffer.as_slice();
 
